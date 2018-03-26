3ed0ebe92b7d8e49f428af52656d7533a9c5b61c
Lance Jordan <djlancejordan@gmail.com>
2018-03-26 16:05:03 -0400

    add generic data getter setter

--- app/models/soundcloud_user.rb
+++ app/models/soundcloud_user.rb
@@ -1,3 +1,13 @@
 class SoundcloudUser < ApplicationRecord
   serialize :user_hash
+
+  def data
+    OpenStruct.new(self.user_hash)
+  end
+
+  def data=(hash)
+    hash.each do |k,v|
+      self.user_hash[k] = v
+    end
+  end
 end
